ebfc44085015c886e9feab762e5aef61507f3f64
Aman Chouhan <ak6927887@gmail.com>
2025-10-03 23:22:44 +0530

    Update Basic_string.cpp

--- Basic_string.cpp
+++ Basic_string.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    string s="Aman";
+    cout<<s<<endl;
+   /* string s1;
+    cin>>s1; //after space and tab it will not take input
+    cout<<s1<<endl;  */
+    string s2;
+    getline(cin,s2);
+    cout<<s2<<endl;
+    cout<<s2.size()<<endl;   //return size of the s2
+    string s3=s+s2; //Add string s and s2
+    cout<<s3;
+    return 0;
+}
