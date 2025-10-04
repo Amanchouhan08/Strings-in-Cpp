0f27a7fed23a61060e9aba20235f729bb401b866
Aman Chouhan <ak6927887@gmail.com>
2025-10-04 23:55:39 +0530

    Update p04_gfg.cpp

--- p04_gfg.cpp
+++ p04_gfg.cpp
@@ -3,4 +3,16 @@ string sort(string s) {
     // complete the function here
     sort(s.begin(),s.end());
     return s;
+
+ /*
+       for(int i=0;i<s.size()-1;i++){
+         for(int j=i+1;j<s.size();j++){
+             if(s[j]<s[i]){
+                 swap(s[j],s[i]);
+              }
+          }
+       }
+     return s;
+*/
+    
 }
